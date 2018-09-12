9d614ed7c9b510274984748fafd03a50e3c70963
Alexey Chernikov <alexey.chernikov@dev.zodiac.tv>
2018-09-12 14:03:19 +0300

    Using embedded spacemacs lsp layer, and keybindings are borrowed from unmerged pull request from spacemacs upstream

--- packages.el
+++ packages.el
@@ -11,9 +11,7 @@
 
 (defconst spacemacs-lsp-cquery-packages
   '(
-    cquery
-    company-lsp
-    lsp-ui
+    (cquery :requires company company-lsp lsp-mode lsp-ui)
     ))
 
 (defun spacemacs-lsp-cquery/init-cquery ()
@@ -24,39 +22,94 @@
     :config
     (progn
       (setq cquery-executable lsp-cquery-executable)
+
+      (cquery-use-default-rainbow-sem-highlight)
+
+      (defun spacemacs//c-c++-lsp-call-function (prefix suffix &rest args)
+	  (apply (spacemacs//c-c++-lsp-symbol prefix suffix) args))
+
+      (defun spacemacs//c-c++-lsp-funcall-interactively (prefix suffix &rest args)
+	  (funcall-interactively (spacemacs//c-c++-lsp-symbol prefix suffix) args))
+
+      (defun spacemacs//c-c++-lsp-funcall-interactively-no-args (prefix suffix)
+	  (funcall-interactively (spacemacs//c-c++-lsp-symbol prefix suffix)))
+
+      (defun spacemacs//c-c++-lsp-set-symbol (prefix suffix value)
+	  (set (spacemacs//c-c++-lsp-symbol prefix suffix) (symbol-value value)))
+
+      (defun c-c++/callers () (interactive) (lsp-ui-peek-find-custom 'callers (spacemacs//c-c++-lsp-string "$" "/callers")))
+      (defun c-c++/vars () (interactive) (lsp-ui-peek-find-custom 'vars (spacemacs//c-c++-lsp-string "$" "/vars")))
+
+      (defun c-c++/references-address ()
+	(interactive)
+	(lsp-ui-peek-find-custom
+	  'address "textDocument/references"
+	  (plist-put (lsp--text-document-position-params) :context
+		     '(:role 128))))
+
+      (defun c-c++/references-read ()
+	(interactive)
+	(lsp-ui-peek-find-custom
+	  'read "textDocument/references"
+	  (plist-put (lsp--text-document-position-params) :context
+		     '(:role 8))))
+
+      (defun c-c++/references-write ()
+	(interactive)
+	(lsp-ui-peek-find-custom
+	  'write "textDocument/references"
+	  (plist-put (lsp--text-document-position-params) :context
+		     '(:role 16))))
+
+    (defun spacemacs//c-c++-lsp-cquery-customise-lsp-ui-peek ()
+        (defun c-c++/base () (interactive) (lsp-ui-peek-find-custom 'base "$cquery/base")))
+
+    (defun spacemacs//c-c++-lsp-ccls-customise-lsp-ui-peek ()
+        (defun c-c++/base ()
+	      (interactive)
+	          (lsp-ui-peek-find-custom 'base "$ccls/inheritanceHierarchy"
+					         (append (lsp--text-document-position-params) '(:flat t :level 3)))))
+
+      (defun c-c++/call-hierarchy () (interactive) (spacemacs//c-c++-lsp-funcall-interactively nil "-call-hierarchy" nil))
+      (defun c-c++/call-hierarchy-inv () (interactive) (spacemacs//c-c++-lsp-funcall-interactively nil "-call-hierarchy" t))
+      (defun c-c++/inheritance-hierarchy () (interactive) (spacemacs//c-c++-lsp-funcall-interactively nil "-inheritance-hierarchy"))
+      (defun c-c++/inheritance-hierarchy-inv () (interactive) (spacemacs//c-c++-lsp-funcall-interactively nil "-inheritance-hierarchy" t))
+      (defun c-c++/member-hierarchy () (interactive) (spacemacs//c-c++-lsp-funcall-interactively-no-args nil "-member-hierarchy"))
+      (defun c-c++/freshen-index () (interactive) (spacemacs//c-c++-lsp-funcall-interactively nil "-freshen-index"))
+      (defun c-c++/preprocess-file () (interactive) (spacemacs//c-c++-lsp-funcall-interactively nil "-preprocess-file"))
+      (dolist (mode c-c++-modes)
+	(spacemacs/set-leader-keys-for-major-mode mode
+						  ;; goto
+						  ;; FIXME gf conflicts with `lsp-ui-flycheck-list' binding in lsp layer
+						  ;; Better corrected in lsp-layer? rebinding here for now
+						  "g." #'lsp-ui-peek-find-definitions
+						  "g," #'lsp-ui-peek-find-references
+						  "g[" #'lsp-ui-peek-jump-backward
+						  "g]" #'lsp-ui-peek-jump-forward
+						  "ge" #'lsp-ui-flycheck-list
+						  "gf" 'find-file-at-point
+						  "gF" 'ffap-other-window
+						  "g&" #'c-c++/references-address
+						  "gR" #'c-c++/references-read
+						  "gW" #'c-c++/references-write
+						  "gc" #'c-c++/callers
+						  "gv" #'c-c++/vars
+						  ;; help/hierarchy
+						  "hb" #'c-c++/base ;;Replace this with lsp-goto-implementation in lsp-layer?
+						  "hc" #'c-c++/call-hierarchy
+						  "hC" #'c-c++/call-hierarchy-inv
+						  "hi" #'c-c++/inheritance-hierarchy
+						  "hI" #'c-c++/inheritance-hierarchy-inv
+						  "hm" #'c-c++/member-hierarchy
+						  ;; lsp/backend
+						  "lf" #'c-c++/freshen-index
+						  "lp" #'c-c++/preprocess-file
+						  "ll" #'lsp-ui-imenu
+						  "lr" #'lsp-rename
+						  ))
+
+      (spacemacs|add-company-backends :backends company-lsp :modes c-mode-common)
+      (setq cquery-extra-init-params '(:completion (:detailedLabel t)))
     )))
-
-(defun spacemacs-lsp-cquery/init-company-lsp ()
-  (use-package company-lsp
-	       :init
-	       (progn
-		 (spacemacs|add-company-backends :backends company-lsp :modes c-mode-common)
-		 (setq company-transformers nil company-lsp-async t company-lsp-cache-candidates nil)
-		 (setq cquery-extra-init-params '(:completion (:detailedLabel t)))	
-		 )))
-
-(defun spacemacs-lsp-cquery/init-lsp-ui ()
-  (use-package lsp-ui
-	       :init
-	       (progn
-		 (dolist (mode '(c-mode c++-mode))
-		   (evil-leader/set-key-for-mode mode
-						 "g." #'lsp-ui-peek-find-definitions
-						 "g," #'lsp-ui-peek-find-references
-						 "g[" #'lsp-ui-peek-jump-backward
-						 "g]" #'lsp-ui-peek-jump-forward
-						 "qb" (lambda () (interactive) (lsp-ui-peek-find-custom 'base "$cquery/base"))
-						 "qc" (lambda () (interactive) (lsp-ui-peek-find-custom 'base "$cquery/callers"))
-						 "qd" (lambda () (interactive) (lsp-ui-peek-find-custom 'base "$cquery/derived"))
-						 "qv" (lambda () (interactive) (lsp-ui-peek-find-custom 'base "$cquery/vars"))
-						 "R"  #'cquery-freshen-index
-						 "hm" #'cquery-member-hierarchy
-						 "hi" #'cquery-inheritance-hierarchy
-						 "hI" (lambda () (interactive) (cquery-inheritance-hierarchy t))
-						 "hc" #'cquery-call-hierarchy
-						 "hC" (lambda () (interactive) (cquery-call-hierarchy t))
-						 "ll" #'lsp-ui-imenu
-						 "lr" #'lsp-rename
-						 )))))
 
 ;;; packages.el ends here
